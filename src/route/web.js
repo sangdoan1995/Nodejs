8397cf655886630b1857bd805781730136171912
sang <sangdoan123456789@gmail.com>
2023-06-23 11:25:37 +0700

    update fronend

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,7 @@ let router = express.Router();
 let initWebRoutes = (app)=>{
     router.get('/', homecontroller.getHomePage);
     router.get('/about', homecontroller.getAboutPage);
+    router.get('/crud', homecontroller.getCRUD);
 
     return app.use("/",router);
 
